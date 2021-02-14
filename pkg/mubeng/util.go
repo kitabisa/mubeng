21df3b10186e390b7d3e5a5caecdd65b8bdef051
Dwi Siswanto <dwi.siswanto98@gmail.com>
2021-02-14 15:59:52 +0700

    Fix invalid address for SOCKS5 dialer proxy & missing proxy scheme branch for auto-switch transport

--- pkg/mubeng/util.go
+++ pkg/mubeng/util.go
@@ -2,34 +2,37 @@ package mubeng
 
 import (
 	// "crypto/tls"
+	"fmt"
 	"net/http"
 	"net/url"
 
 	"golang.org/x/net/proxy"
 )
 
-// Transport is switching transport between HTTP or SOCKS5 proxies
-// depending on the protocol scheme, returning value of http.Transport with Dialer or Proxy.
+// Transport to auto-switch transport between HTTP/S or SOCKSv5 proxies.
+// Depending on the protocol scheme, returning value of http.Transport with Dialer or Proxy.
 func Transport(p string) (tr *http.Transport, err error) {
 	proxyURL, err := url.Parse(p)
 	if err != nil {
 		return nil, err
 	}
 
-	dialer, err := proxy.SOCKS5("tcp", p, nil, nil)
+	dialer, err := proxy.SOCKS5("tcp", proxyURL.Host, nil, proxy.Direct)
 	if err != nil {
 		return nil, err
 	}
 
-	switch proxyURL.String() {
+	switch proxyURL.Scheme {
 	case "socks5":
 		tr = &http.Transport{
 			Dial: dialer.Dial,
 		}
-	default:
+	case "http", "https":
 		tr = &http.Transport{
 			Proxy: http.ProxyURL(proxyURL),
 		}
+	default:
+		return nil, fmt.Errorf("unsupported proxy protocol scheme: %s", proxyURL.Scheme)
 	}
 
 	tr.DisableKeepAlives = true
