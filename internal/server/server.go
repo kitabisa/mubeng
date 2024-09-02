dcfd2fae8773e91fc437c419a0543947b648809d
Dwi Siswanto <git@dw1.io>
2024-09-03 04:49:13 +0700

    feat(server): handle `logo.Open` err
    also initiate `NewLogger` with slices of receivers.
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- internal/server/server.go
+++ internal/server/server.go
@@ -16,13 +16,19 @@ import (
 // An active log have 2 receivers, especially stdout and into file if opt.Output isn't empty.
 // Then close the proxy server if it receives a signal that interrupts the program.
 func Run(opt *common.Options) {
+	var recs []*logo.Receiver
+
 	cli := logo.NewReceiver(os.Stderr, "")
 	cli.Color = true
 	cli.Level = logo.DEBUG
+	recs = append(recs, cli)
 
-	file, _ := logo.Open(opt.Output)
-	out := logo.NewReceiver(file, "")
-	out.Format = "%s: %s"
+	file, err := logo.Open(opt.Output)
+	if err == nil {
+		out := logo.NewReceiver(file, "")
+		out.Format = "%s: %s"
+		recs = append(recs, out)
+	}
 
 	dump = &httpretty.Logger{
 		RequestHeader:  true,
@@ -43,7 +49,7 @@ func Run(opt *common.Options) {
 		Handler: handler.HTTPProxy,
 	}
 
-	log = logo.NewLogger(cli, out)
+	log = logo.NewLogger(recs...)
 
 	if opt.Watch {
 		watcher, err := opt.ProxyManager.Watch()
