4c7176265eecdb62dfaf6b262ef3f53e52440e03
Dwi Siswanto <git@dw1.io>
2024-09-07 08:36:17 +0700

    feat(mubeng): rm global `client` var
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pkg/mubeng/vars.go
+++ pkg/mubeng/vars.go
@@ -1,20 +1,14 @@
 package mubeng
 
-import "net/http"
-
-var (
-	client *http.Client
-
-	// HopHeaders are meaningful only for a single transport-level connection, and are not stored by caches or forwarded by proxies.
-	HopHeaders = []string{
-		"Connection",
-		"Keep-Alive",
-		"Proxy-Authenticate",
-		"Proxy-Authorization",
-		"Proxy-Connection",
-		"Te", // canonicalized version of "TE"
-		"Trailers",
-		"Transfer-Encoding",
-		"Upgrade",
-	}
-)
+// HopHeaders are meaningful only for a single transport-level connection, and are not stored by caches or forwarded by proxies.
+var HopHeaders = []string{
+	"Connection",
+	"Keep-Alive",
+	"Proxy-Authenticate",
+	"Proxy-Authorization",
+	"Proxy-Connection",
+	"Te", // canonicalized version of "TE"
+	"Trailers",
+	"Transfer-Encoding",
+	"Upgrade",
+}
