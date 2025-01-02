c7133c42083a3475e9a82baaf8793c076e6abd2e
Dwi Siswanto <git@dw1.io>
2025-01-02 16:58:49 +0700

    feat(mubeng): handles AWS URL proxy
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pkg/mubeng/transport.go
+++ pkg/mubeng/transport.go
@@ -6,28 +6,37 @@ import (
 	"net/http"
 	"net/url"
 
+	"github.com/kitabisa/mubeng/pkg/helper/awsurl"
 	"h12.io/socks"
 )
 
 // Transport to auto-switch transport between HTTP/S or SOCKS v4(A) & v5 proxies.
-// Depending on the protocol scheme, returning value of http.Transport with Dialer or Proxy.
-func Transport(p string) (tr *http.Transport, err error) {
-	proxyURL, err := url.Parse(p)
-	if err != nil {
-		return nil, err
+//
+// Depending on the protocol scheme, returning value of [http.Transport] with
+// [http.Transport.Dialer] or [http.Transport.Proxy]. If protocol scheme is "aws",
+// it will return default [http.Transport].
+func Transport(p string) (*http.Transport, error) {
+	var proxyURL *url.URL
+	var err error
+
+	tr := new(http.Transport)
+
+	if awsurl.IsURL(p) {
+		return tr, fmt.Errorf("%w: %w", ErrUnsupportedProxyProtocolScheme, ErrSwitchTransportAWSProtocolScheme)
+	} else {
+		proxyURL, err = url.Parse(p)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	switch proxyURL.Scheme {
 	case "socks4", "socks4a", "socks5":
-		tr = &http.Transport{
-			Dial: socks.Dial(p),
-		}
+		tr.Dial = socks.Dial(p)
 	case "http", "https":
-		tr = &http.Transport{
-			Proxy: http.ProxyURL(proxyURL),
-		}
+		tr.Proxy = http.ProxyURL(proxyURL)
 	default:
-		return nil, fmt.Errorf("unsupported proxy protocol scheme: %s", proxyURL.Scheme)
+		return nil, fmt.Errorf("%w: %s", ErrUnsupportedProxyProtocolScheme, proxyURL.Scheme)
 	}
 
 	tr.DisableKeepAlives = true
